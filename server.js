e9e40b19d5d4feb6cbb18bd27caf5d12202602df
melissayang <melissayang888@gmail.com>
2021-05-29 15:49:44 +0700

    PokemonCatcher Version 1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +21,16 @@ mongoose
 
 //Require route
 app.use('/', require('./routes/myPokemonRoute'));
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/dist'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
