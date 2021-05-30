eb080d529c4eaafae3b2a2313c05d7cda94eb283
melissayang <melissayang888@gmail.com>
2021-05-30 21:45:25 +0700

    Remove unnecessary minified css

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,23 +1,11 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
     output: {
         filename: "main.js",
         path: path.join(__dirname, "/dist"),
-    },
-    module : {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            }
-        ]
     }
 });
--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -2,9 +2,6 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -12,27 +9,7 @@ module.exports = merge(common, {
         filename: "main.[hash].js",
         path: path.join(__dirname, "/dist"),
     },
-    optimization: {
-       minimizer: [
-            new OptimizeCssAssetsPlugin(),
-            new TerserPlugin()
-        ]
-    },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].[hash].css"
-        }),
         new CleanWebpackPlugin()
-    ],
-    module : {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            }
-        ]
-    }
+    ]
 });
